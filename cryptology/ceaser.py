a52d9a0816822e1ed56afce99f06f44f3b4b6b2a
Vedant36 <vedantneema@gmail.com>
2021-12-28 16:48:46 +0530

    fixed ceasar cipher

--- cryptology/ceaser.py
+++ cryptology/ceaser.py
@@ -9,7 +9,7 @@ qwe=[]
 for shift in range(0,26):
   qwe=[]
   for i in asd:
-    if ord(i) in range(97,97+25):
+    if ord(i) in range(97,97+26):
       qwe.append(chr((ord(i)+shift)%26+97))
     else: qwe.append(i)
 
